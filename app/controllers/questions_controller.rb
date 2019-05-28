b9c36a501e809b1891425980d1b67c1c926a2d6e
Emily Weidt <emily.weidt@mail.de>
2019-05-28 16:36:31 -0400

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,7 @@
 class QuestionsController < ApplicationController
   def ask
-
   end
 
   def answer
-    @question = params[:ask_type]
-    @answer = coach_answer
-  end
-
-  def coach_answer
-    if @question.include?('?')
-      'Silly question, get dressed and go to work!'
-    elsif @question == 'I am going to work'
-      'Great!'
-    else "I don't care, get dressed and go to work!"
-    end
   end
 end
